ff204135362fd7eaf328825f03856f99987eb37a
Nat Budin <natbudin@gmail.com>
2008-12-16 16:02:36 +0000

    Fix attached image path

--- app/models/attached_image.rb
+++ app/models/attached_image.rb
@@ -1,4 +1,5 @@
 class AttachedImage < ActiveRecord::Base
-  has_attached_file :image
+  has_attached_file :image,
+    :path => ":rails_root/public/attached_images/:id/:style_:filename"
   belongs_to :site_template
 end
