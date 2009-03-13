48ddcd199caffc0b9294a4a166e9daf5d6161661
Nat Budin <natbudin@gmail.com>
2009-03-13 18:03:57 +0000

    Attendee tracking

--- app/helpers/agenda_helper.rb
+++ app/helpers/agenda_helper.rb
@@ -1,2 +1,8 @@
 module AgendaHelper
+  def context_events(person)
+    Attendance.find_all_by_person_id(person.id, :include => :event, :order => "start").select do |a|
+      e = a.event
+      (not e.nil?) and (@context.nil? or e.parent == @context)
+    end
+  end
 end
