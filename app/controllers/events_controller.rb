5074e9a44dc671465d5be512fa4e6807dbc73f74
Nat Budin <natbudin@gmail.com>
2009-09-30 13:21:52 -0400

    Get rid of deprecated non-restful event functions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -60,7 +60,6 @@ class EventsController < ApplicationController
     if @context
       @event.parent = @context
     end
-    calculate_edit_vars
   end
   
   def create
@@ -105,7 +104,6 @@ class EventsController < ApplicationController
   
   def edit
     @event = Event.find params[:id]
-    calculate_edit_vars
   end
   
   def update
@@ -118,7 +116,6 @@ class EventsController < ApplicationController
     if @context
       @event.parent = @context
     end
-    calculate_edit_vars
   end
   
   def submit_proposal
@@ -150,104 +147,12 @@ class EventsController < ApplicationController
     end
     
     flash[:error_messages] ||= []
-    
-    if params[:locations]
-      locs = params[:locations]
-      @event.event_locations.each do |booking|
-        if not locs.include? booking.location.id
-          booking.destroy
-        end
-      end
-
-      locs.each do |locid|
-        loc = Location.find(locid)
-        if not @event.locations.include? Location.find(locid)
-          booking = EventLocation.new(:event => @event, :location => loc, :exclusive => true)
-          if not booking.save
-            flash[:error_messages].push("Could not add the location #{loc.name}: #{booking.errors.full_messages.join(", ")}")
-          end
-        end
-      end    
-    end
-    
-    if params[:remove_staff]
-      params[:remove_staff].each_key do |staff_id|
-        if params[:remove_staff][staff_id]
-          @event.staff.delete Person.find(staff_id.to_i)
-        end
-      end
-    end
-    
-    if not params[:add_staff].blank?
-      person_id = params[:add_staff].sub(/^(\D+)/, "")
-      staffer = Person.find(person_id)
-      a = Attendance.new :person => staffer, :event => @event, :is_staff => true, :counts => false
-      if not a.save
-        flash[:error_messages].push("Could not add the staff member specified: #{a.errors.full_messages.join(", ")}")
-      end
-    end
 
     if @event.kind_of? ProposedEvent
       if not @event.staff.include? @event.proposer
         a = Attendance.new :person => @event.proposer, :event => @event, :is_staff => true, :counts => false
         if not a.save
           flash[:error_messages].push("Could not add the event proposer as a staff member: #{a.errors.full_messages.join(", ")}")
-        end
-      end
-    end
-    
-    if not params[:add_public_info_field].blank?
-      pif = @event.public_info_fields.new :name => params[:add_public_info_field]
-      if not pif.save
-        flash[:error_messages].push("Couldn't create new PublicInfoField: #{pif.errors.full_messages.join(", ")}")
-      end
-    end
-    
-    if params[:delete_public_info_field]
-      params[:delete_public_info_field].each_key do |pif_id|
-        if params[:delete_public_info_field][pif_id]
-          @event.public_info_fields.find(pif_id.to_i).destroy
-        end
-      end
-    end
-    
-    if params[:add_virtual_site] and not params[:add_virtual_site][:domain].blank?
-      site_template_id = params[:add_virtual_site][:site_template]
-      if site_template_id and site_template_id != ''
-        site_template = SiteTemplate.find(site_template_id)
-      end
-      vs = @event.virtual_sites.new :domain => params[:add_virtual_site][:domain], :site_template => site_template
-      if not vs.save
-        flash[:error_messages].push("Couldn't create new VirtualSite: #{vs.errors.full_messages.join(", ")}")
-      end
-    end
-    
-    if params[:delete_virtual_site]
-      params[:delete_virtual_site].each_key do |vs_id|
-        if params[:delete_virtual_site][vs_id]
-          @event.virtual_sites.find(vs_id.to_i).destroy
-        end
-      end
-    end
-    
-    if params[:limited_capacity]
-      if (params[:limited_capacity] == "true" and not (@event.kind_of? LimitedCapacityEvent))
-        @event.type = "LimitedCapacityEvent"
-        @event.save
-        @event = Event.find(@event.id)
-      elsif (params[:limited_capacity] == "false" and (@event.kind_of? LimitedCapacityEvent))
-        @event.type = "Event"
-        @event.save
-        @event = Event.find(@event.id)
-      end
-    end
-    
-    if @event.kind_of? LimitedCapacityEvent and params[:limits]
-      %w(male female neutral).each do |gender|
-        slot = @event.attendee_slots.find_or_initialize_by_gender(gender)
-        slot.update_attributes(params[:limits][gender])
-        if not slot.save
-          flash[:error_messages].push("Could not change #{gender} attendee limits: #{slot.errors.full_messages.join(", ")}")
         end
       end
     end
@@ -266,31 +171,6 @@ class EventsController < ApplicationController
         format.xml  { render :xml => @event.errors.to_xml }
       end
     end
-  end
-  
-  def calculate_edit_vars
-    @limited_capacity = @event.kind_of? LimitedCapacityEvent
-    @limits = {}
-    %w(male female neutral).each do |gender|
-      @limits[gender] = {}
-      slot = nil
-      if @limited_capacity
-        slot = @event.attendee_slots.find_by_gender(gender)
-      end
-      %w(min preferred max).each do |threshold|
-        if slot.nil?
-          limit = 0
-        else
-          limit = slot.send(threshold)
-        end
-        @limits[gender][threshold] = limit
-      end
-    end
-    
-    @registration_open = @event.registration_open
-    @non_exclusive = @event.non_exclusive
-    @age_restricted = @event.age_restricted
-    @min_age = @event.min_age
   end
   
   def check_edit_permissions
