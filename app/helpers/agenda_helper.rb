786257201582ac6d0e83f9d57ef4b7d3a2bf35d0
Nat Budin <natbudin@gmail.com>
2010-03-23 11:44:16 -0400

    Don't show unconfirmed proposed events

--- app/helpers/agenda_helper.rb
+++ app/helpers/agenda_helper.rb
@@ -2,7 +2,7 @@ module AgendaHelper
   def context_events(person)
     Attendance.find_all_by_person_id(person.id, :include => :event, :joins => :event, :order => "start").select do |a|
       e = a.event
-      (not e.nil? or e.kind_of?(ProposedEvent)) and (@context.nil? or e.parent == @context)
+      (not e.nil?) and (not e.kind_of?(ProposedEvent)) and (@context.nil? or e.parent == @context)
     end
   end
 end
