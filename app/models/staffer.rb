af9eed8436de9d9bcea6f945cfdd4303e11db670
Nat Budin <natbudin@gmail.com>
2010-07-28 13:54:05 -0400

    Support for new finder method in Staffer

--- app/models/staffer.rb
+++ app/models/staffer.rb
@@ -2,4 +2,9 @@ class Staffer < ActiveRecord::Base
   belongs_to :event
   belongs_to :person
   acts_as_list :scope => :event
+  validates_uniqueness_of :person_id, :scope => :event_id
+  
+  scope :in_event_and_ancestors, lambda { |event|
+    where(:event_id => ([event] + event.ancestors).map(&:id))
+  }
 end
