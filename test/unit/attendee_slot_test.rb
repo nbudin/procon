29d466445d4d3148ce0329b2159ba0b08e766acc
Nat Budin <natbudin@gmail.com>
2011-01-11 14:05:11 -0500

    Add a sanity check to make sure the override methods aren't screwing up normal behavior

--- test/unit/attendee_slot_test.rb
+++ test/unit/attendee_slot_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class AttendeeSlotTest < ActiveSupport::TestCase
+  def test_fields
+    slot = AttendeeSlot.new(:max => 3, :min => 1, :preferred => 2)
+    assert_equal 1, slot.min
+    assert_equal 3, slot.max
+    assert_equal 2, slot.preferred
+  end
+
   def test_nil_defaults_to_zero
     slot = AttendeeSlot.new(:max => nil, :min => nil, :preferred => nil)
     assert_equal 0, slot.min
