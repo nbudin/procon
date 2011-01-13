4f09ad6d0b6efbd682003a68cc6e8b6c4b6d9db6
Nat Budin <natbudin@gmail.com>
2011-01-13 16:53:33 -0500

    More checks in heroku initializer

--- config/initializers/heroku_external_db.rb
+++ config/initializers/heroku_external_db.rb
@@ -10,14 +10,16 @@ if !ActiveRecord::Base.configurations['users'] && ENV['USERS_DATABASE_URL']
     :host => uri.host
   }
 
+  db_config[:port] = uri.port unless uri.port.nil?
+
   if ENV['USERS_DATABASE_CA']
     db_config[:sslca] = File.join(ca_path, ENV['USERS_DATABASE_CA'])
+    raise "#{db_config[:sslca]} does not exist!" unless File.exists?(db_config[:sslca])
   end
   ActiveRecord::Base.configurations['users'] = db_config
 end
 
 if ENV['DATABASE_CA']
+  raise "#{ENV['DATABASE_CA']} does not exist!" unless File.exists?(ENV['DATABASE_CA'])
   ActiveRecord::Base.configurations[ENV['RAILS_ENV']][:ssl_ca] = File.join(ca_path, ENV['DATABASE_CA'])
 end
-
-raise ActiveRecord::Base.configurations.inspect
