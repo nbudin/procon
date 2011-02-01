b051c389e588e212c347f2bfc33f27ea0cba4bd3
Nat Budin <natbudin@gmail.com>
2011-02-01 15:01:43 -0500

    Deploy with bundler

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+require 'bundler/capistrano'
+
 # This defines a deployment "recipe" that you can feed to capistrano
 # (http://manuals.rubyonrails.com/read/book/17). It allows you to automate
 # (among other things) the deployment of your application.
