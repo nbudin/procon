10c659dfe877bbea84212287d5dca06ea0b4caab
Nat Budin <natbudin@gmail.com>
2011-02-01 14:58:53 -0500

    Undebug

--- config/initializers/heroku_external_db.rb
+++ config/initializers/heroku_external_db.rb
@@ -1,6 +1,4 @@
 if ENV['EXTERNAL_DATABASE_URL']
   HerokuExternalDb.setup_rails_env!
   HerokuExternalDb.setup_configuration!("USERS", "users")
-  
-  raise ActiveRecord::Base.configurations.inspect
 end
