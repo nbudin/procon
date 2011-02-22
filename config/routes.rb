d3b2fe35e49bb53c6af76cd65e017dc88cbb0d7e
Nat Budin <natbudin@gmail.com>
2011-02-22 11:43:17 -0500

    Hook up named root url

--- config/routes.rb
+++ config/routes.rb
@@ -19,9 +19,7 @@ ActionController::Routing::Routes.draw do |map|
   # map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
   # This route can be invoked with purchase_url(:id => product.id)
 
-  # You can have the root of your site routed by hooking up '' 
-  # -- just remember to delete public/index.html.
-  map.connect '', :controller => "main"
+  map.root :controller => "main"
   
   map.resources :events, :collection => {:schedule => :get, :propose => :get, :submit_proposal => :post}, :member => {:available_people => :get} do |events|
     events.resources :attendances, :collection => { :children => :get }
