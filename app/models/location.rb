28de0cc83d7bfbeb9a9ae740238ace300aa346d5
Nat Budin <natbudin@gmail.com>
2011-10-01 17:08:27 -0400

    Merge in location fix from laptop

--- app/models/location.rb
+++ app/models/location.rb
@@ -7,9 +7,6 @@ class Location < ActiveRecord::Base
   scope :shareable, :include => :event_locations, :conditions => [ "event_locations.exclusive = ?", false ]
   
   def self.roots(locs)
-    roots = locs.dup
-    redundant = roots.select { |l| roots.include? l.parent }
-    roots -= redundant
-    return roots
+    locs.reject { |l| locs.include? l.parent }
   end
 end
