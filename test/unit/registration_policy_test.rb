4dcd2d067fa543c9c42bab9dd84c387b7e465e50
Nat Budin <natbudin@gmail.com>
2011-12-06 14:52:18 -0500

    Don't reference fixtures that don't exist

--- test/unit/registration_policy_test.rb
+++ test/unit/registration_policy_test.rb
@@ -1,8 +1,6 @@
 require 'test_helper'
 
 class RegistrationPolicyTest < ActiveSupport::TestCase
-  fixtures :registration_policies
-
   # Replace this with your real tests.
   def test_truth
     assert true
--- test/unit/virtual_site_test.rb
+++ test/unit/virtual_site_test.rb
@@ -1,8 +1,6 @@
 require 'test_helper'
 
 class VirtualSiteTest < ActiveSupport::TestCase
-  fixtures :virtual_sites
-
   # Replace this with your real tests.
   def test_truth
     assert true
