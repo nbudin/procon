c24208b7f71b3f0f9bf0e31c9a71a5df248a63d1
Nat Budin <nbudin@patientslikeme.com>
2014-04-11 10:50:07 -0700

    Optimize agenda/attendees a bit

--- app/helpers/agenda_helper.rb
+++ app/helpers/agenda_helper.rb
@@ -1,6 +1,7 @@
 module AgendaHelper
-  def context_events(person)
-    Attendance.find_all_by_person_id(person.id, :include => :event, :joins => :event, :order => "start").select do |a|
+  def context_events(person, attendances=nil)
+    attendances ||= Attendance.find_all_by_person_id(person.id, :include => :event, :joins => :event)
+    attendances.sort_by { |att| att.event.start || Time.at(0) }.select do |a|
       e = a.event
       (not e.nil?) and (not e.kind_of?(ProposedEvent)) and (@context.nil? or e.parent == @context)
     end
