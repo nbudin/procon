98b95cc8a3fb0deb7a83c7d0f248266b641da7e3
Nat Budin <nbudin@patientslikeme.com>
2015-03-04 09:38:39 -0500

    Refactor a bit and fix a bug

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,7 +1,11 @@
 class Ability
   include CanCan::Ability
   
+  attr_reader :person
+  
   def initialize(person)
+    @person = person
+    
     alias_action [:show_description], :to => :read
     
     can :read, Event
@@ -13,8 +17,6 @@ class Ability
         can :manage, :all
       else
         # TODO: generalize this to work with multiple event levels deep
-        
-        staff_event_ids = Event.all(:joins => :attendances, :conditions => ["attendances.person_id = ? AND attendances.is_staff = ? AND type != 'ProposedEvent'", person.id, true], :select => "events.id").map(&:id)
         
         can :manage, Event, :id => staff_event_ids
         can :view_attendances, Event, :attendees_visible => true, :attendances => { :person_id => person.id }
@@ -31,6 +33,16 @@ class Ability
         can :manage, ProposedEvent, :parent_id => staff_event_ids
         can [:read, :update, :destroy], ProposedEvent, :proposer_id => person.id
       end
+    end
+  end
+  
+  def staff_event_ids
+    @staff_event_ids ||= begin
+      Event.all(
+        :joins => :attendances, 
+        :conditions => ["attendances.person_id = ? AND attendances.is_staff = ? AND (type != 'ProposedEvent' OR type IS NULL)", person.id, true], 
+        :select => "events.id"
+      ).map(&:id)
     end
   end
 end
